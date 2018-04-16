c486e3947abc91008fd0adcb7e153556709421f0
中野 真明貴 <s01720@CG3114.local>
2018-04-16 15:59:39 +0900

    Implements Comparable
    ShapeクラスがComparableを実装するように変更
    実際に動かしていないので、正しく動かないor順番が逆の可能性あり

--- OCPDemo.java
+++ OCPDemo.java
@@ -1,13 +1,18 @@
-abstract class Shape{
+abstract class Shape implements Comparable<Shape>{
    abstract public void Draw();
+   protected double priority;
+   public int compareTo(Shape other){
+      return new Integer(priority).compareTo(other.priority);
+   }
 }
 
 abstract class Circle extends Shape {
    private int radius;
    private Point center;
    public Circle(int radius, Point center) {
-      this.radius = radius;
-      this.center = center;
+       this.priority = 1D;
+       this.radius = radius;
+       this.center = center;
    }
 }
 
@@ -15,8 +20,9 @@ abstract class Square extends Shape {
    private int size;
    private Point topLeft;
    public Square(int size, Point topLeft) {
-      this.size = size;
-      this.topLeft = topLeft;
+       this.priority = 2D;
+       this.size = size;
+       this.topLeft = topLeft;
    }
 }
 
@@ -40,8 +46,7 @@ class BigSquare extends Square {
 
 class DrawingTool {
     public void DrawAllShapes(List<Shape> shapeList) {
-        shapeList.stream().filter(shape -> shape instanceof Circle).forEach(Shape::Draw);
-        shapeList.stream().filter(shape -> shape instanceof Square).forEach(Shape::Draw);
+        shapeList.stream().sort().forEach(Shape::Draw);
     }
 }
 
